5085c3dd9f038e08055b049ef081db2ec2cb4a65
dnlKaizer <danilokaizer@gmail.com>
2024-09-28 12:03:11 -0300

    add: classe BoardView

---
+++ ProjetoXadrez/src/br/xadrez/view/BoardView.java
@@ -0,0 +1,22 @@
+package br.xadrez.view;
+
+import br.xadrez.model.Board;
+import br.xadrez.model.Position;
+import br.xadrez.model.pieces.Piece;
+
+public class BoardView {
+
+    public void printBoard(Board board) {
+        System.out.println();
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                Piece piece = board.getPieceAt(
+                    Position.create(row, col)
+                );
+                if (piece == null) System.out.print("-\t");
+                else System.out.print(piece.getName() + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
