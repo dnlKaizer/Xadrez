a9d81d2259a7e25c27eaa229de20ff03530a3da9
dnlKaizer <danilokaizer@gmail.com>
2024-09-28 08:44:56 -0300

    add: classe Piece

---
+++ ProjetoXadrez/src/br/xadrez/model/pieces/Piece.java
@@ -0,0 +1,39 @@
+package br.xadrez.model.pieces;
+
+import br.xadrez.model.Color;
+import br.xadrez.model.Position;
+
+public abstract class Piece implements Cloneable {
+    
+    protected Color color;
+    protected Position position;
+
+    protected Piece(Color color, Position position) {
+        this.color = color;
+        this.position = position;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+    public Position getPosition() {
+        return position;
+    }
+    
+    public void setPosition(Position position) {
+        if (position != null) this.position = position;
+    }
+
+    public abstract String getName();
+
+    @Override
+    public Piece clone() {
+        try {
+            return (Piece) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+}
