01537d381843e41c5fa6e55a103ba5cc50921e79
dnlKaizer <danilokaizer@gmail.com>
2024-09-29 09:56:19 -0300

    alter: removendo responsabilidade de criação de tabuleiro de Board + removendo métodos não utilizados

---
+++ ProjetoXadrez/src/br/xadrez/model/board/Board.java
@@ -0,0 +1,64 @@
+package br.xadrez.model.board;
+
+import br.xadrez.model.Direction;
+import br.xadrez.model.Position;
+import br.xadrez.model.pieces.*;
+
+public class Board {
+    
+    private Piece[][] board;
+    
+    protected Board(Piece[][] board) {
+        this.board = board;
+    }
+
+    /**
+     * Retorna um clone da peça na posição
+     * 
+     * @param position {@code Position} da peça
+     * @return {@code Piece} na posição do parâmetro ou {@code null} se a posição não for válida ou não tiver peça
+      */
+    public Piece getPieceAt(Position position) {
+        Piece piece = board[position.getRow()][position.getCol()];
+        if (piece == null) return null;
+        else return piece.clone(); 
+    }
+
+    /**
+     * Verifica se não existe peça bloqueando o caminho.
+     * 
+     * @param from {@code Position} da peça
+     * @param to {@code Position} de destino
+     * @return {@code true} se o caminho está livre, {@code false} se não
+      */
+    public boolean isPathClear(Position from, Position to) {
+        Direction direction = Direction.create(from, to);
+        if (direction == null) return false;
+
+        // Lógica para verificar os caminhos intermediários
+        Position between = direction.getNextPosition(from);
+        while (!to.equals(between)) {
+            if (getPieceAt(between) != null) return false;
+            between = direction.getNextPosition(between);
+        }
+        
+        if (isDestinationClear(from, to)) return true;
+        else return false;
+    }
+
+    /**
+     * Verifica se existe uma peça de mesma cor no destino
+     * 
+     * @param from {@code Position} da peça
+     * @param to {@code Position} de destino
+     * @return {@code true} se o destino estiver livre ou se 
+     * existir peça de cor oposta, {@code false} se não
+      */
+    public boolean isDestinationClear(Position from, Position to) {
+        if (to == null) return false;
+        Piece pieceFrom = getPieceAt(from);
+        Piece pieceTo = getPieceAt(to);
+        return pieceTo == null || (!pieceFrom.getColor().equals(pieceTo.getColor()));
+    }
+
+}
