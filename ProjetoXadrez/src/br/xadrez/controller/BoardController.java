de020d2bf55123018ca8914dd2b3c9e7c36d9329
dnlKaizer <danilokaizer@gmail.com>
2024-10-02 11:13:48 -0300

    add: melhorias no código

--- ProjetoXadrez/src/br/xadrez/controller/BoardController.java
+++ ProjetoXadrez/src/br/xadrez/controller/BoardController.java
@@ -1,22 +1,28 @@
 package br.xadrez.controller;
 
-import br.xadrez.model.board.Board;
-import br.xadrez.model.board.BoardBuilder;
+import br.xadrez.model.Color;
+import br.xadrez.model.board.*;
+import br.xadrez.model.pieces.*;
 import br.xadrez.view.BoardView;
 
 public class BoardController {
     BoardBuilder boardBuilder;
     Board board;
     BoardView boardView;
+    Color turn;
 
-    public BoardController(Board board, BoardView boardView) {
+    public BoardController(Board board, BoardView boardView, BoardBuilder boardBuilder, Color turn) {
         this.board = board;
         this.boardView = boardView;
-        this.boardBuilder = new BoardBuilder();
+        this.boardBuilder = boardBuilder;
+        this.turn = turn;
     }
-    
-    public void setUpBoard() {
+
+    public BoardController() {
+        this.boardView = new BoardView();
+        this.boardBuilder = new BoardBuilder();
         this.board = boardBuilder.buildStandard();
+        this.turn = Color.WHITE;
     }
 
     public void printBoard() {
--- ProjetoXadrez/src/br/xadrez/main/App.java
+++ ProjetoXadrez/src/br/xadrez/main/App.java
@@ -1,28 +1,16 @@
 package br.xadrez.main;
 
+import org.fusesource.jansi.AnsiConsole;
+
 import br.xadrez.controller.BoardController;
-import br.xadrez.model.Color;
-import br.xadrez.model.Position;
-import br.xadrez.model.board.Board;
-import br.xadrez.model.board.BoardBuilder;
-import br.xadrez.model.pieces.*;
-import br.xadrez.view.BoardView;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        BoardBuilder boardBuilder = new BoardBuilder();
-        boardBuilder.standard();
-        boardBuilder.placePiece(
-            Position.create(3, 4),
-            PieceFactory.createPiece(Pawn.class, Color.WHITE, Position.create(6, 4))
-        );
-        Board board = boardBuilder.build();
-        board.move(Position.create(1, 3), Position.create(3, 3));
-        BoardController boardController = new BoardController(board, new BoardView());
+        AnsiConsole.systemInstall();
+
+        BoardController boardController = new BoardController();
         boardController.printBoard();
-        Piece piece = board.getPieceAt(Position.create(3, 4));
-        for (Position position : piece.getValidMoves(board)) {
-            System.out.println(position.toString());
-        }
+
+        AnsiConsole.systemUninstall();
     }
 }
